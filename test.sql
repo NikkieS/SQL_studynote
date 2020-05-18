6e02b49fb98acc46339e2acaa589a72a7dceb4eb
a4-01 <21400389@handong.edu>
2020-05-18 16:43:25 +0900

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,3 +1,18 @@
-select * from emp;
+select * from emp order by sal desc;
 
+select * from dept;
+
+select job, sal, empno from emp;
+
+-- emp : 사원정보
+-- dept : 부서정보
+-- bonus : 임시테이블
+-- salgrade : 급여 테이블
+
+-- 테이블의 구조 확인 : desc 테이블이름
+desc emp;
+desc dept;
+desc salgrade;
+
+select * from emp;
 select * from dept;
