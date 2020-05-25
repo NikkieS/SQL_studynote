e706559becf8f68732de4be9b075305e1812185c
a4-01 <21400389@handong.edu>
2020-05-25 13:07:51 +0900

    Update practice2_0522.sql

--- practice2_0522.sql
+++ practice2_0522.sql
@@ -1,27 +1,46 @@
+select * from customer c, orders o, book b where c.custid=o.custid and o.bookid=b.bookid;
+
 -- 1 마당서점의고객이요구하는다음질문에대해SQL 문을작성하시오.
 -- (5) 박지성이구매한도서의출판사수
 select bookid from customer natural join orders where name='박지성';
 
-select count(publisher) as numofpublisher 
+select count(distinct publisher) as numofpublisher 
 from book 
-where bookid in (select bookid from customer natural join orders where name='박지성');
+where bookid in (select distinct bookid from customer natural join orders where name='박지성');
+
+select count(distinct publisher) as numofpublisher
+from customer c, orders o, book b 
+where c.custid=o.custid and o.bookid=b.bookid and c.name='박지성';      -- 통합 테이블
 
 -- (6) 박지성이구매한도서의이름, 가격, 정가와판매가격의차이
 select bookname, price, price - (select saleprice from orders o where o.bookid=b.bookid) as diff 
 from book b
 where bookid in (select bookid from customer natural join orders where name='박지성');
 
+select b.bookname, b.price, b.price-o.saleprice as diff
+from customer c, orders o, book b 
+where c.custid=o.custid and o.bookid=b.bookid and c.name='박지성';     -- 통합 테이블 
+
 -- (7) 박지성이구매하지않은도서의이름
 select bookname 
 from book 
 where bookid not in (select bookid from customer natural join orders where name='박지성');
 
+select b.bookname
+from customer c, orders o, book b 
+where c.custid=o.custid and o.bookid=b.bookid and c.name<>'박지성';
+
 -- 2 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
 -- (8) 주문하지않은고객의이름(부속질의사용)
 select custid from orders;  -- 주문한 고객
+
 select name 
 from customer 
 where custid not in (select custid from orders);
+
+select c.name 
+from orders o, customer c 
+where o.custid(+)=c.custid and o.orderid is null;
 
 -- (9) 주문금액의총액과주문의평균금액
 select sum(saleprice), avg(saleprice) from orders;
@@ -45,7 +64,7 @@ select *
 from (select orderid, price-saleprice as diff from book b, orders o where b.bookid=o.bookid order by diff desc)
 where rownum=1; -- 첫번째
 
--- (13) 도서의 판매액 평균보다 자신의구매액 평균이 더 높은 고객의이름
+-- (13) 도서의 판매액 평균보다 자신의 구매액 평균이 더 높은 고객의 이름
 select avg(saleprice) 
 from orders;   -- 도서 평균 판매액
 
