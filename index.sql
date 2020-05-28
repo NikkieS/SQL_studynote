ccbb005f2791f006db096bbfcf47cd402298ae80
a4-01 <21400389@handong.edu>
2020-05-28 18:21:54 +0900

    Create index.sql

---
+++ index.sql
@@ -0,0 +1,26 @@
+-- =============================================================
+-- INDEX
+-- =============================================================
+-- 검색을 빠르게 하기위해 사용한다.
+-- DML 작업에는 성능 저하가 올 수 있다
+
+desc user_ind_columns;
+
+select index_name, column_name, table_name
+from user_ind_columns;
+
+drop table emp01;
+
+create table emp01
+as
+select * from emp;
+
+insert into emp01 select * from emp01;
+select count(*) from emp01;
+
+insert into emp01(empno, ename) values(1111, 'SON');
+insert into emp01(empno, ename) values(2222, 'PARK');
+select * from emp01 where ename='PARK';
+
+-- INDEX 생성
+-- create index 인덱스이름 on 테이블이름 (컬럼이름)
